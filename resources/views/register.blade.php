5f4976f594201e9126f389b2f9a5516e8529b945
Dario Contreras <thedaroo81@gmail.com>
2023-04-20 17:36:41 -0400

    Melody 0.1.1
    Cambios en posiciones de la interfaz

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,14 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="icons/MelodyLogo.png">
     <title>Melody</title>
-    <link rel="stylesheet" href="../css/app.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="formulario">
-        <h1><image id="logo" src="icons/MelodyLogo.png" width="40" height="40" aling = "center" > Melody</h1>
-        <p>¡Inicia sesión!</p>
+        <h1><image src="icons/MelodyLogo.png" class = "img" width="40" height="40" > Melody</h1>
+        <p>¡Crea tu cuenta!</p>
         <form methos="post">
             <div class="username">
+                <div><label>NOMBRE DE USUARIO</label></div>
+                <input type="text" required>     
+            </div>
+            <div class="email">
                 <div><label>CORREO ELECTRÓNICO</label></div>
                 <input type="text" required>                
             </div>
@@ -21,8 +25,8 @@
                 <div><label>CONTRASEÑA</label></div>
                 <input type="text" required>     
             </div>
-            <div class="register"><a href="register.blade.php">¿No tienes cuenta? ¡Registrate aquí!</a></div>
-            <input type="submit" value="INGRESAR">
+            <div class="register"><a href="a">¿Ya tienes cuenta? ¡Inicia sesión aquí!</a></div>
+            <input type="submit" value="REGISTRAR">
         </form>
     </div>
 </body>
