45d8d6748213fbfd212517976bd6c1e8ea3cf483
Nicolas Robles <nicolas.robles@alumnos.ucn.cl>
2023-05-07 19:29:06 -0400

    Cambios nico
    completar la vista del cuerpo de la pagina de inicio cliente

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<link href="{{ asset('assets/styles.css') }}" rel="stylesheet" type="text/css">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pagina inicio</title>
+</head>
+
+<body>
+    <section>
+        <h1> Recomendados </h1>
+        <div class="container"> 
+    </section>
+
+    </div>
+    <div class="container">
+
+        <div>
+            <img src="assets/ticket.png" alt="Concierto" width="150" height="150" align="center">
+            <h2>The Weeknd</h2>
+            <h3>"Super Bowl Half Time Show"</h3>
+            <p>25 Abril 2023</p>
+            <p>Valor: $30.000 CLP</p>
+            <p>Entradas Disponibles: 300</p>
+
+            <button class="buttonBuy">COMPRAR</button>
+        </div>
+
+        <div>
+            <img src="assets/ticket.png" alt="Concierto" width="150" height="150" align="center">
+            <h2>Chancho en Piedra</h2>
+            <h3>"Grandes exitos de ayer y oink!"</h3>
+            <p>30 Septiembre 2023</p>
+            <p>Valor: $10.000 CLP</p>
+            <p>Entradas Disponibles: 1000</p>
+            
+            <button class="buttonBuy">COMPRAR</button>
+        </div>
+
+        <div>
+            <img src="assets/ticket.png" alt="Concierto" width="150" height="150" align="center">
+            <h2>Los Jaivas</h2>
+            <h3>"Super Bowl Half Time Show"</h3>
+            <p>18 Septiembre 2023</p>
+            <p>Valor: $15.000 CLP</p>
+            <p>Entradas Disponibles: 300</p>
+
+            <button class="buttonBuy">COMPRAR</button>
+        </div>
+
+        <div>
+            <img src="assets/ticket.png" alt="Concierto" width="150" height="150 align="center">
+            <h2>Lollapalooza</h2>
+            <h3>"2023 EDITION"</h3>
+            <p>15 Julio 2023</p>
+            <p>Valor: $100.000 CLP</p>
+            <p>Entradas Disponibles: 0</p>
+            
+            <button class="buttonSpend">AGOTADO</button>
+        </div>
+
+        <div>
+            <img src="assets/ticket.png" alt="Concierto" width="150" height="150" align="center">
+            <h2>Chico Trujillo</h2>
+            <h3>"European Invasion"</h3>
+            <p>20 Junio 2023</p>
+            <p>Valor: $50.000 CLP</p>
+            <p>Entradas Disponibles: 100</p>
+            
+            <button class="buttonBuy">COMPRAR</button>
+        </div>
+
+        <div>
+            <img src="assets/ticket.png" alt="Concierto" width="150" height="150" align="center">
+            <h2> Calvin Harris</h2>
+            <h3> "Sunny Hill Festival"</h3>
+            <p>58 Febrero 2024</p>
+            <p>Valor: $150.000 CLP</p>
+            <p>Entradas Disponibles: 0</p>
+            
+            <button class="buttonSpend">AGOTADO</button>
+        </div>
+    </div>
+</html>
