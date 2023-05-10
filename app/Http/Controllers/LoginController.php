6e7e208cb33e328e06bf891c6c240fca84b91b6e
Victor Araya <112587750+VitokoUcn@users.noreply.github.com>
2023-05-09 22:13:04 -0400

    Update LoginController.php
    Validacion login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,4 +9,21 @@ class LoginController extends Controller
     public function index(){
         return view('auth.login');
     }
+
+    public function store(Request $request)
+    {
+        $message = makeMessage();
+        // Validate
+        $this->validate($request, [
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:8']
+        ], $message);
+
+        // Se intenta validar al usuario
+        if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
+            return back()->with('message', 'Los datos ingresados son incorrectos');
+        }
+
+        return redirect()->route('dashboard');
+    }
 }
