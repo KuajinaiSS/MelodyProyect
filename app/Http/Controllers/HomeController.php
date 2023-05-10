b4abff63c7127005f3c9149ab47811b7aef30adb
KuajinaiSS <kuajinaiss@gmail.com>
2023-05-09 21:40:51 -0400

    arregle idioma

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('inicio');
+        return view('home');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\RegisterController;
 */
 
 Route::get('/', function () {
-    return view('inicio');
+    return view('home');
 });
 
 
