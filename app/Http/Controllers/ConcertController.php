9286b1300202f668439c4749eea16927d815f081
Dario Contreras <thedaroo81@gmail.com>
2023-05-24 17:52:13 -0400

    Melody v0.5.2
    Se arreglaron los errores de la iteración 1

--- app/Http/Controllers/ConcertController.php
+++ app/Http/Controllers/ConcertController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\Concert;
 use Illuminate\Http\Request;
 
@@ -24,22 +25,13 @@ class ConcertController extends Controller
         //Create Error message
         $message = makeMessage();
 
-
-
         //Validate inputs
         $this->validate($request, [
             'concertName'=> ['required','min:5','max:2147483648'],
             'price' => ['required','numeric','min:20000','max:2147483647'],
             'stock' => ['required','numeric','between:100,400'],
-            'date' => ['required','date']
+            'date' => ['required','date','after:today']
         ], $message);
-
-
-        //verify date format
-        $invalidDate = validDate($request->date);
-        if($invalidDate){
-            return back()->with('message', 'La fecha debe ser mayor a '. date("d-m-Y"));
-        }
 
         $existConcert = existConcertDay($request->date);
         if($existConcert){
@@ -53,7 +45,7 @@ class ConcertController extends Controller
             'date' => $request->date
         ]);
 
-        return back()->with('confirmMessage','Concierto creado con exito');
+        return back()->with('confirmMessage','Concierto creado con éxito');
 
 
     }
