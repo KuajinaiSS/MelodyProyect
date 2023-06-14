e7205447d6ebc7ad65c4168c0b230a734e40373a
Victor Araya <112587750+VitokoUcn@users.noreply.github.com>
2023-06-13 22:25:19 -0400

    Revert "Revert "Merge branch 'vitoko' into rey""
    This reverts commit ed8da0979b578f5de0a5fadca0130b96b281fb2d.

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,14 +1,14 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+<link href="{{asset('assets/styles.css')}}" rel="stylesheet" type="text/css" >
+<form action="action_page.php">
+  <body >
+  <div class="container">
+    
+    <h1> <img src =  "assets/Logo-Melody.png" alt="Melody" width="60" height="60" align = "center"> Melody</h1>
+    <p align = center>¡Crea tu cuenta!</p>
+    
+    
+    <label for="email"><b>NOMBRE DE USUARIO</b></label>
+    <input type="text" placeholder=" " name="usuario" id="email" required>
 
     <label for="email"><b>CORREO ELECTRÓNICO</b></label>
     <input type="text" placeholder=" " name="email" id="email" required>
@@ -16,16 +16,14 @@
 
     <label for="psw"><b>CONTRASEÑA</b></label>
     <input type="password" placeholder=" " name="psw" id="psw" required>
-</div>
+
     <button type="submit" class="registerbtn">REGISTRAR</button>
   </div>
 
   <div class="container signin">
     <p>¿Ya tienes una cuenta? <a href="#">¡Inicia sesión aquí!</a>.</p>
   </div>
-
+  
 </body>
 <footer> <p>  Melody. Derechos reservados 2023</p></footer>
 </form>
-
-@endsection
