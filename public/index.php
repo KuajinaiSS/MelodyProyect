e22345129cc4818cb327fc8636598e8bdf3b6b51
Nicolas Robles <nicolas.robles@alumnos.ucn.cl>
2023-07-07 23:19:49 -0400

    3.0.2
    Corrección de Commit

--- public/index.php
+++ public/index.php
@@ -31,6 +31,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 require_once __DIR__ . '/../app/Helpers/MyHelper.php'; 
+
 require __DIR__.'/../vendor/autoload.php';
 
 /*
