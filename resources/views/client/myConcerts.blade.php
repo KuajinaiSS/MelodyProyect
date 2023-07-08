5f2df462af018cdda1c088176a0809b9b94f2a85
KuajinaiSS <kuajinaiss@gmail.com>
2023-07-08 02:46:28 -0400

    V3.2.5
    Requerimiento RE-001 Terminado

--- resources/views/client/myConcerts.blade.php
+++ resources/views/client/myConcerts.blade.php
@@ -6,125 +6,139 @@
 @vite('resources/css/table.css')
 
 @section('content')
-    <h1 class="titulo">Conciertos</h1>
+    @auth
+        @vite('resources/css/table.css')
+        @if (auth()->user()->role === 0)
+            <img src="{{ asset('img/marker.png') }}" class="marker" width="25" height="6">
+            <h1 class="tittle">Mis Conciertos</h1>
+
+
+            {{-- Si no a comprado ningun concierto --}}
+            @if ($detailsOrders->count() === 0)
+                <p class="errorMsg" style="text-align: center">No hay conciertos por mostrar</p>
+            @endif
+
+            @if ($detailsOrders->count() > 0)
+                <table>
+
+                    <thead>
+                        <tr>
+                            <th>Número de reserva</th>
+                            <th>Nombre del Concierto</th>
+                            <th>Fecha del concierto</th>
+                            <th>Fecha de compra</th>
+                            <th>Cantidad de entradas</th>
+                            <th>Total pagado</th>
+                            <th>Medio de pago</th>
+                            <th>Comprobante</th>
+                        </tr>
+                    </thead>
+
+
+                    <tbody>
+                        @foreach ($detailsOrders as $detailOrder)
+                            <tr>
+                                {{-- Número de reserva --}}
+                                <td>
+                                    <p>
+                                        {{ $detailOrder->reservation_number }}
+                                    </p>
+
+                                </td>
+
+
+                                {{-- Nombre del concierto --}}
+                                <td>
+                                    <p>
+                                        {{ $detailOrder->concertDates->concertName }}
+                                    </p>
+                                </td>
 
 
 
-    <table>
-
-        <thead>
-            <tr>
-                <th>Número de reserva</th>
-                <th>Nombre del Concierto</th>
-                <th>Fecha del concierto</th>
-                <th>Fecha de compra</th>
-                <th>Cantidad de entradas</th>
-                <th>Total pagado</th>
-                <th>Medio de pago</th>
-                <th></th>
-            </tr>
-        </thead>
-
-
-        <tbody>
-            @foreach ($detailsOrders as $detailOrder)
-            <tr>
-                {{-- Número de reserva --}}
-                <td>
-                    <p>
-                        {{ $detailOrder->reservation_number }}
-                    </p>
-
-                </td>
-
-
-                {{-- Nombre del concierto --}}
-                <td>
-                    <p>
-                        {{ $detailOrder->concertDates->concertName }}
-                    </p>
-                </td>
+                                {{-- Fecha del concierto --}}
+                                <td>
+                                    <p>
+                                        {{ date('d/m/Y', strtotime($detailOrder->concertDates->date)) }}
+                                    </p>
+                                </td>
 
 
 
-                {{-- Fecha del concierto --}}
-                <td>
-                    <p>
-                        {{ date('d/m/Y',strtotime($detailOrder->concertDates->date)) }}
-                    </p>
-                </td>
+                                {{-- Fecha de compra --}}
+                                <td>
+                                    <p>
+                                        {{ date('d/m/Y', strtotime($detailOrder->voucher->date)) }}
+                                    </p>
+                                </td>
 
 
 
-                {{-- Fecha de compra --}}
-                <td>
-                    <p>
-                        {{ $detailOrder->voucher->detail_order_id }}
-                        {{-- {{ date('d/m/Y',strtotime($detailOrder->voucher->date)) }} --}}
-                    </p>
-                </td>
+                                {{-- Cantidad de entradas compradas/disponibles/actuales(?) --}}
+                                <td>
+                                    <p>
+                                        {{ $detailOrder->quantity }}
+                                    </p>
+                                </td>
 
 
 
-                {{-- Cantidad de entradas compradas/disponibles/actuales(?) --}}
-                <td>
-                    <p>
-                        {{ $detailOrder->quantity }}
-                    </p>
-                </td>
+                                {{-- Total pagado --}}
+                                <td>
+                                    <p>
+                                        ${{ $detailOrder->total }}
+                                    </p>
+                                </td>
 
 
 
-                {{-- Total pagado --}}
-                <td>
-                    <p>
-                        ${{ $detailOrder->total }}
-                    </p>
-                </td>
+                                {{-- Medio de pago --}}
+                                <td>
+                                    <p>
+                                        @if ($detailOrder->payment_method === 1)
+                                            <p>
+                                                Efectivo
+                                            </p>
+                                        @elseif ($detailOrder->payment_method === 2)
+                                            <p>
+                                                Transferencia
+                                            </p>
+                                        @elseif ($detailOrder->payment_method === 3)
+                                            <p>
+                                                Tarjeta de Crédito
+                                            </p>
+                                        @elseif ($detailOrder->payment_method === 4)
+                                            <p>
+                                                Tarjeta de Débito
+                                            </p>
+                                        @endif
+                                    </p>
+                                </td>
 
 
 
-                {{-- Medio de pago --}}
-                <td>
-                    <p>
-                        @if ($detailOrder->payment_method === 1)
-                            <p>
-                                Efectivo
-                            </p>
-                        @elseif ($detailOrder->payment_method === 2)
-                            <p>
-                                Transferencia
-                            </p>
-                        @elseif ($detailOrder->payment_method === 3)
-                            <p>
-                                Tarjeta de Crédito
-                            </p>
-                        @elseif ($detailOrder->payment_method === 4)
-                            <p>
-                                Tarjeta de Débito
-                            </p>
-                        @endif
-                    </p>
-                </td>
+                                <td>
+                                    <a href="{{ route('pdf.descargar', ['id' => $detailOrder->voucher->id]) }}">
+                                        <button class="buttonDetail">
+                                            descargar
+                                        </button>
+                                    </a>
+
+                                </td>
+                            </tr>
+                        @endforeach
+
+                    <tfoot>
+                        <th class="finalRow"> </th>
+                    </tfoot>
 
 
+                    </tbody>
 
-                <td>
-                    <a href="{{ route('admin.sellsDetail') }}">
-                        <button class="buttonDetail">Ver Detalle</button>
-                    </a>
-
-                </td>
-            </tr>
-            @endforeach
-
-            <tfoot>
-                <th class="finalRow"> </th>
-            </tfoot>
-
-
-        </tbody>
-
-    </table>
-
+                </table>
+            @endif
+        @elseif(auth()->user()->role === 0)
+            <meta http-equiv="refresh" content="0;{{ route('viewHome') }}">
+        @endif
+    @endauth
 @endsection
